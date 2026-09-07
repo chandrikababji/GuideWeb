cdc1227a4cb9a4d9a2f333f6672444d7fbfcdc7a
babjiasha1@gmail.com <123@gmail.com>
2026-09-07 10:57:56 +0530

    Point frontend to live Render backend

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ loginForm.addEventListener("submit", async function(event) {
     const password = document.getElementById("password").value;
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/login", {
+        const response = await fetch("https://guideweb-1.onrender.com/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
