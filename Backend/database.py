2cab64cc41750d06ba41641c806bee303d534238
babjiasha1@gmail.com <123@gmail.com>
2026-09-07 10:32:55 +0530

    connect to aiven

--- Backend/database.py
+++ Backend/database.py
@@ -1,15 +1,13 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.engine import URL
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-url_object = URL.create(
-    "mysql+pymysql",
-    username="root",
-    password="root@0320",   # raw password, no encoding needed here
-    host="localhost",
-    port=3306,
-    database="guidesphere",
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"ssl": {"ssl_mode": "REQUIRED"}}
 )
-engine = create_engine(url_object)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
